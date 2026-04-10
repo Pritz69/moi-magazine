0e8a0173c23a1bf17854e323162f6d3cba813fae
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2026-04-10 19:23:46 +0530

    Update API URL to use environment variable

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -35,7 +35,7 @@ export default function Contact() {
       formData.append("instagram", instagram);
       formData.append("message", message);
 
-      const res = await axios.post("http://localhost:8000/contact", formData);
+      const res = await axios.post("process.env.NEXT_PUBLIC_API_URL/contact", formData);
 
       setStatus(res.data.msg);
 
